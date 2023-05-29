b938a353bb43b00f88b9b4e903b7cfea588906fa
Audi Bramastya Oroh <audibram14@gmail.com>
2023-05-29 15:46:55 +0700

    Initial commit

---
+++ src/main/java/com/projects/autoforexbackend/AutoforexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projects.autoforexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoforexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoforexBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/autoforexbackend/AutoforexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.autoforexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoforexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
